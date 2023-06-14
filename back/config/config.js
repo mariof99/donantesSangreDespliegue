a1af508539e28d1ff9e66381f3c7930ee75773ef
mariof99 <marioferestevez@gmail.com>
2023-06-14 15:29:16 +0200

    primer despliegue 3

--- back/config/config.js
+++ back/config/config.js
@@ -5,21 +5,21 @@ module.exports ={
     "username": process.env.DB_USER,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_NAME,
-    "host": process.env.HOST,
+    "host": process.env.DB_HOST,
     "dialect": process.env.DB_DIALECT
   },
   "test": {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_NAME,
-    "host": process.env.HOST,
+    "host": process.env.DB_HOST,
     "dialect": process.env.DB_DIALECT
   },
   "production": {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_NAME,
-    "host": process.env.HOST,
+    "host": process.env.DB_HOST,
     "dialect": process.env.DB_DIALECT
   }
 }
